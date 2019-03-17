201f35446cce99063faea7534e6864cad651a964
Gosha Maruzhenko <gmaruzhe@opendns.com>
2019-03-17 15:15:55 -0700

    charge ready for testing

--- src/server.rb
+++ src/server.rb
@@ -6,15 +6,12 @@ require 'sinatra'
 require 'stripe'
 require 'json'
 
-#TODO make stripe api key initilized from request once stripe connect is ready
+# Our secret api key for logging customers in our account
 Stripe.api_key = "sk_test_dUndr7GHsaxgYD9o9jxn6Kmy"
 
-def log_info(message)
-  puts "\n" + message + "\n\n"
-  return message
-end
 
 helpers do
+  # Json parser with error check
   def json_params
     begin
       JSON.parse(request.body.read)
@@ -22,40 +19,39 @@ helpers do
       halt 400, { message:'Invalid JSON' }.to_json
     end
   end
+  # send log info to console for debugging
+  def log_info(message)
+    puts "\n" + message + "\n\n"
+    return message
+  end
 end
 
-
+# Test endpoint to check if server is up
 get '/' do
   status 200
-  return log_info("Connection Successful")
+  return log_info("Connection Successful\n")
 end
-#
+
 # Create a customer in our platform account
-#
 get '/make_customer' do
   customer = Stripe::Customer.create()
   status 201  #successful in creating a stripe customer
   return log_info( customer[:id]+"\n")
 end
 
-#
 #Creates a charge on a stripe connected account
-#
-
 post '/charge' do
-  json_recieved = json_params
+  json_received = json_params
 
-  #TODO make token request first between customer and store
   token = Stripe::Token.create({
-                                   :customer => json_recieved['customer_id'],
-                               }, {:stripe_account => json_recieved['store_account_id']})
+                                   :customer => json_received['customer_id'],
+                               }, {:stripe_account => json_received['CONNECTED_STRIPE_ACCOUNT_ID']})
 
   charge = Stripe::Charge.create({
-                                     amount: json_recieved['amount'],
+                                     amount: json_received['amount'],
                                      currency: "cad",
                                      source: token.id,
                                      application_fee_amount: 123,
-                                 }, stripe_account: json_recieved['store_account_id'])
+                                 }, stripe_account: json_received['CONNECTED_STRIPE_ACCOUNT_ID'])
   status 201
-
 end
