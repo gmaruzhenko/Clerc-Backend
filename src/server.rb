5bea14474d817f11e0e26676f81c421596584596
Gosha Maruzhenko <gmaruzhe@opendns.com>
2019-03-18 09:02:26 -0700

    started merging and simplifying

--- src/server.rb
+++ src/server.rb
@@ -5,16 +5,22 @@
 require 'sinatra'
 require 'stripe'
 require 'json'
+require 'unirest'
+
+FIREBASE_PROJ_ID = "paywithclerc" #TODO this should be an environment variable
+STRIPE_API_SECRET = "sk_test_dsoNrcwd0QnNHt8znIVNpCJK" # TODO this should also be an environment variable
+STRIPE_CONNECTED_ACCT_URL = "https://connect.stripe.com/oauth/token"
+Stripe.api_key = STRIPE_API_SECRET
 
 # Our secret api key for logging customers in our account (comment to switch accounts during debugging)
 # Account name = Test1
 #Stripe.api_key = "sk_test_dUndr7GHsaxgYD9o9jxn6Kmy"
 # Account name = Sample
-Stripe.api_key = "sk_test_dsoNrcwd0QnNHt8znIVNpCJK"
+#Stripe.api_key = "sk_test_dsoNrcwd0QnNHt8znIVNpCJK"
 
 # configure to run as server
 # for local testing comment out line below
-set :bind, '0.0.0.0'
+#set :bind, '0.0.0.0'
 
 helpers do
   # Json parser with error check
@@ -62,4 +68,60 @@ post '/charge' do
                                      application_fee_amount: 123,
                                  }, stripe_account: json_received['CONNECTED_STRIPE_ACCOUNT_ID'])
   status 201
+end
+
+# This is called by front-end once the connected account is authorized
+# Once the business gives us authorization, frontend will receive an AUTHORIZATION_CODE
+# which is then passed to this method. We will use the AUTHORIZATION_CODE to retrieve credentials for the business
+post '/create-standard-account' do
+
+  # Get the authorization code & cast to string
+  json_input=json_params
+  id = json_input['id']
+  authCode = json_input['account_auth_code']
+  log_info(json_input.to_s)
+
+  # Check that it's not empty, otherwise continue
+  if authCode.empty? || id.empty?
+    halt 400, "Invalid request - Missing parameters"
+  end
+
+  # Retrieve required fields from Stripe
+  #    curl https://connect.stripe.com/oauth/token \
+  #   -d client_secret=sk_test_BGUip2DhwDI2yHRBHQPTQK7Q \
+  #   -d code="{AUTHORIZATION_CODE}" \
+  #   -d grant_type=authorization_code
+  stripeData = {
+      :client_secret => STRIPE_API_SECRET,
+      :code => authCode,
+      :grant_type=> 'authorization_code'
+  }
+  stripeResponse = Unirest.post STRIPE_CONNECTED_ACCT_URL, parameters: stripeData
+  # for debugging
+  puts stripeData
+  # Check that we have a returned success
+  if stripeResponse.code != 200
+      halt 400, "Something went wrong"
+
+  # Response is valid, store informaton specific to the retailer in firestore
+  # {
+  #     "token_type": "bearer",
+  #     "stripe_publishable_key": "{PUBLISHABLE_KEY}",
+  #     "scope": "read_write",
+  #     "livemode": false,
+  #     "stripe_user_id": "{ACCOUNT_ID}",
+  #     "refresh_token": "{REFRESH_TOKEN}",
+  #     "access_token": "{ACCESS_TOKEN}"
+  #   }
+
+  # TODO save this stuff in firestore
+  #puts stripeResponse.raw_body
+  #puts stripeResponse.code
+  #puts stripeResponse.body
+  log_info(stripeResponse)
+
+  # If all this is done and good, return a success message
+  log_info("Success!")
+  status 201
+  end
 end
